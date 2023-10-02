b3294222e5f5b72b5470f29b9c15d2fbfd197bfb
Magdalena Samuel <magdalenasamuel4@gmail.com>
2023-10-02 12:34:26 -0700

    Initial Commit

---
+++ CryptoPulse/CryptoPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPulseApp.swift
+//  CryptoPulse
+//
+//  Created by Magdalena Samuel on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
